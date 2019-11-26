b8fc9aa558f67a5168dd79f270114590bc378a26
Tuxtampa <37031351+Tuxtampa@users.noreply.github.com>
2019-11-26 14:27:33 +0100

    Merge try#1
    Seems to work but git reports conflicts so here we go

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,12 @@
+import java.io.IOException;
 
 public class main {
+    public String verse1;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         gui run = new gui();
         run.initializeGUI();
+        powerpoint slide1 = new powerpoint();
+        slide1.generateSlide(400);
     }
 }
